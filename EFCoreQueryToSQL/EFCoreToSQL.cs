72c5c552676b5af82175ccf60495ebc65841f184
pascx64 <pascx64@gmail.com>
2020-02-23 11:40:05 -0500

    added source

---
+++ EFCoreQueryToSQL/EFCoreToSQL.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EFCoreQueryToSQL
+{
+    public static class EFCoreToSQL
+    {
+        public static string ToSql<TEntity>(IQueryable<TEntity> query) where TEntity : class
+        {
+            var enumerator = query.Provider.Execute<IEnumerable<TEntity>>(query.Expression).GetEnumerator();
+            var relationalCommandCache = Private(enumerator, "_relationalCommandCache");
+            var selectExpression = Private<SelectExpression>(relationalCommandCache, "_selectExpression");
+            var factory = Private<IQuerySqlGeneratorFactory>(relationalCommandCache, "_querySqlGeneratorFactory");
+
+            var sqlGenerator = factory.Create();
+            var command = sqlGenerator.GetCommand(selectExpression);
+
+            string sql = command.CommandText;
+            return sql;
+        }
+
+        private static object Private(object obj, string privateField) => obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+        private static T Private<T>(object obj, string privateField) => (T)obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+
+    }
+}
